18f46de21cf4a38934c9f96b4505226b7fac398c
theanhtr <trtheanh.work@gmail.com>
2022-12-24 10:07:48 +0700

    fix database

--- database/migrations/2022_12_16_143826_create_batches_table.php
+++ database/migrations/2022_12_16_143826_create_batches_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->integer('quantity');
             $table->string('date');
             $table->unsignedBigInteger('range_id');
-            $table->foreign('range_id')->references('user_id')->on('ranges')->onDelete('cascade');
+            $table->foreign('range_id')->references('id')->on('ranges')->onDelete('cascade');
             $table->unsignedBigInteger('factory_id');
             $table->foreign('factory_id')->references('user_id')->on('factories')->onDelete('cascade');
             $table->timestamps();
